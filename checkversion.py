37769aff42c6d7624012f36c5d6ffb28f7705bac
nshillin <nshillin@ualberta.ca>
2017-01-10 09:03:53 -0700

    updated checkversion.py

--- checkversion.py
+++ checkversion.py
@@ -4,6 +4,6 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/nshillin/cmput404lab1/master/checkversion.py")
 print response.status_code
 print response.text
